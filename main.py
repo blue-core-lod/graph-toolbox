2cfa19d9a257bd440631c7c12676277608da1834
Jeremy Nelson <jermnelson@gmail.com>
2025-07-03 10:00:56 -0700

    feat: UI Stubs for Batch, Keycloak Env and Login

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 import asyncio
 import sys
 import js
-
+import pathlib
+import tomllib
 
 from pyodide.ffi import create_proxy
 
 import helpers
 
 import rdflib
+
+
+FILE = pathlib.Path(".")
 
 BIBFRAME = rdflib.Namespace("http://id.loc.gov/ontologies/bibframe/")
 SINOPIA = rdflib.Namespace("http://sinopia.io/vocabulary/")
@@ -18,10 +22,17 @@ from sinopia_api import show_groups
 from load_rdf import bibframe_sparql as bf_sparql_widget, build_graph, download_graph
 from query_rdf import download_query_results, run_query
 
-
 bf_sparql_widget("bf-sparql-query")
 
-helpers.set_versions(__version__)
+pyproject = FILE.parent / "pyproject.toml"
+
+if pyproject.exists():
+    with pyproject.open('rb') as fo: 
+        project = tomllib.load(fo)
+        version = project["project"]["version"]
+else:
+    version = __version__
+helpers.set_versions(version)
 
 splash_modal_close_btn = js.document.getElementById("splashModalCloseBtn")
 splash_modal_close_btn.click()
--- src/helpers.py
+++ src/helpers.py
@@ -22,16 +22,3 @@ async def render_markdown(element_id):
         return
     raw_mkdwn = element.innerText
     element.innerHTML = markdown.markdown(raw_mkdwn)
-
-
-def run_step_01(event):
-    stage_urls = js.document.querySelector("#sinopia-stage-urls")
-    output = js.document.querySelector("#step-01-output")
-    raw_input = stage_urls.value
-    try:
-        exec(raw_input)
-        js.console.log(globals())
-        output.innerHTML = f"sinopia_urls has ${len(sinopia_stage_urls)}"
-    except Exception as e:
-        output.innerHTML = f"Error {e} loading {stage_urls.value}"
- 
