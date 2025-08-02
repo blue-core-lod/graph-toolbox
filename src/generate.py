25c474b3f7cb1c88e6d56d9f32eda351c51919fc
Jeremy Nelson <jermnelson@gmail.com>
2025-08-02 09:14:24 -0700

    feat: Using Jinja templates for UI refactor

---
+++ src/generate.py
@@ -0,0 +1,26 @@
+import argparse
+import logging
+import pathlib
+
+from datetime import datetime, UTC
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader("src/templates"),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    src_path = pathlib.Path(__name__)
+    time_stamp = datetime.now(UTC)
+    logger.info(f"Staring Blue Core Graph Toolkit static site {time_stamp}")
+    index_template = env.get_template("index.html")
+    with (src_path.parent / "index.html").open("w+") as fo:
+        fo.write(index_template.render(timestamp=time_stamp))
+    total_time = (datetime.now(UTC) - time_stamp).seconds
+    logger.info(f"Total time {total_time}")
+
