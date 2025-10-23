cf19078949a7179c604c9dbf50518c493f6e387f
Jeremy Nelson <jermnelson@gmail.com>
2025-10-23 12:05:10 +0200

    feat: Add GraphInfoToolbar Puepy component

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
 
-from puepy import Application, Component, Page, t
-from puepy.core import html
+from puepy import Application, Page, t
 from puepy.router import Router
-
+from components import GraphInfoToolbar
 
 app = Application()
 app.install_router(Router, link_mode=Router.LINK_MODE_HASH)
@@ -10,6 +9,8 @@ app.install_router(Router, link_mode=Router.LINK_MODE_HASH)
 
 @app.page()
 class BlueCoreToolboxPage(Page):
-
     def populate(self):
         t.h1("Graph Toolbox")
+        t.graph_info_toolbar()
+
+app.mount("#app")
