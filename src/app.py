9f246f6fa00b0048a23075066a4786d70a82f85c
Jeremy Nelson <jermnelson@gmail.com>
2025-10-23 12:36:55 +0200

    feat: Components for major sections in the app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 
 from puepy import Application, Page, t
 from puepy.router import Router
-from components import GraphInfoToolbar
 
 app = Application()
 app.install_router(Router, link_mode=Router.LINK_MODE_HASH)
@@ -10,7 +9,15 @@ app.install_router(Router, link_mode=Router.LINK_MODE_HASH)
 @app.page()
 class BlueCoreToolboxPage(Page):
     def populate(self):
-        t.h1("Graph Toolbox")
+        t.navbar()
         t.graph_info_toolbar()
+        with t.div(class_name="row"):
+            with t.div(class_name="col-4"):
+                t.graph_search_query_toolbar()
+            with t.div(class_name="col-7"):
+                t.graph_work_bench()
+            with t.div(class_name="col-1"):
+                t.graph_ops_toolbar()
+        t.app_footer()
 
 app.mount("#app")
